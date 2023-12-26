ca7405655fde5a3daef2a891de08e0fc7c7968b3
Ikram4545 <154882758+Ikram4545@users.noreply.github.com>
2023-12-26 16:58:31 +0000

    04_comparision.js

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("1" < 2);
+// console.log(1 > "3");
+
+
+// console.log("2" === "2");
+// console.log("2" == 2);
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(null <= 0)
+
