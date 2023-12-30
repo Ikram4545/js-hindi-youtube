91e5a5ae330dee09811394c1487c6726bc27fbd1
Ikram4545 <154882758+Ikram4545@users.noreply.github.com>
2023-12-30 17:27:04 +0000

    math

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,71 @@
+const Name = "ikram  "
+const Perctange = 50
+
+// console.log(Name + Perctange);
+
+// console.log(`hello my name is ${Name} my ${Perctange}`);
+
+// console.log(Name + Perctange);
+
+// console.log(`hello my name is ${Name} my ${Perctange}`);
+
+const gameName =new String ('ikram-hc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('r'));
+// console.log(gameName.toLowerCase());
+
+// const NewString = gameName.substring();
+// console.log(NewString);
+
+// const anotherString = gameName.slice(1, -3);
+
+// console.log(anotherString);
+
+const NewStringOne = "    ikram     "
+console.log(NewStringOne);
+
+console.log(NewStringOne.trim());
+
+
+// ++++++++++++++++++++++++++++++++++++youtube code hai ++++++++++++++++++++++++++++++
+
+const name = "Ikram"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ikram-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Ikram    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Ikram.com/Ikram%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('ali hassan'))
+
+console.log(gameName.split('-'));
---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,42 @@
+// const score = 400
+// console.log(score.toString());
+
+// const balance = new Number(300)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3))
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(2));
+
+// const hunderd =
+
+// console.log(hunderd.toLocaleString('EN-IN'));
+
+// max value ya min value ke liye apko browser ka dashboard open krke console mein dekhna hoga 
+// Number Number.MAX_SAFE_INTEGER is terha likhni hoti hai
+
+
+// ++++++++++++++++++++++++++++++++maths+++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math); 
+// console.log(Math.abs(-4)); min waali ko 4 he krke dege consle ke ander or + ko bi kuch nhi krye ge
+// console.log(Math.ceil(4.33)); top se value start hogi 
+// console.log(Math.floor(4.33)); bottom se value start hogi 
+// console.log(Math.round(4.83));  is mein value round ho kr ayege 
+// console.log(Math.sqrt(43.980));
+
+
+// console.log(Math.random()) 0 se 1 ke beach mein value ayege bass is mein 
+// console.log((Math.random()*10))
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10) + 1)
+// console.log(Math.ceil(Math.random()*10) + 1)
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.ceil(Math.random() * (max - min + 1) + min));
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));  
+// (Math.random() * (max - min + 1) + min) yey aik formula hai
+
+
