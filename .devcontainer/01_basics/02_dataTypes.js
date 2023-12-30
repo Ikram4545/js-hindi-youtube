30dce88cee2367abdf4f8a20af4df018990c5f1c
Ikram4545 <154882758+Ikram4545@users.noreply.github.com>
2023-12-30 19:08:53 +0000

    07_datesinJs.js

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("ikram")
 
 
-let name = "hitesh"
+let name = "ikram"
 let age = 18
 let isLoggedIn = false
 let state;
---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,55 @@
+// // Dates
+
+// let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// // // console.log(myDate.toDateString());
+// // console.log(myDate.toLocaleString());
+// // console.log(typeof myDate);
+
+// // ========================myCreateDate==========================
+
+// let myCreateDate = new Date(2023, 1, 23 , 22, 30 ,12,)
+// console.log(myCreateDate.toLocaleString());
+// // console.log(myCreateDate.toDateString());
+
+// // ========================myTimeStamp==========================
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// // console.log(myCreateDate.getTime());
+// // console.log(Math.floor(myTimeStamp.Date()/10000));
+
+
+// // ========================newDate==========================
+
+
+// let newDate = new Date()
+
+// // console.log(newDate.getMilliseconds());
+// // console.log(newDate.getSeconds());
+// // console.log(newDate.getMinutes());
+// // console.log(newDate.getHours());
+// console.log(newDate.getDay());
+// // console.log(newDate.getDate());
+// // console.log(newDate.getMonth() + 1);
+// // console.log(newDate.getMonth());
+// // console.log(newDate.getFullYear());
+
+// // `${newDate.getMonth()}` full date 
+
+
+// // yey same he kaam krta console.log(myDate.toLocaleString()); is ki terha.. 
+// // or agar coustmize krna hai or ache se define krna hai to 
+// // console.log(myDate.toLocaleString());  newDate.toLocaleString('default' ,{
+ 
+// //     weekday : "long"
+
+// // }
+// // )
+
+// // isko use krte hai
+
+// // or alhudulilah yey 01_basics  ka topic complete ho gaya hai ider
+
